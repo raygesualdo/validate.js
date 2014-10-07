76026a54da6f6180a7270b9900c1c69fbf1d493a
Ray Gesualdo <ray@rjgesualdo.com>
2014-10-07 13:12:40 -0400

    added validation rule for phone number

--- validate.js
+++ validate.js
@@ -36,7 +36,8 @@
             valid_base64: 'The %s field must contain a base64 string.',
             valid_credit_card: 'The %s field must contain a valid credit card number.',
             is_file_type: 'The %s field must contain only %s files.',
-            valid_url: 'The %s field must contain a valid URL.'
+            valid_url: 'The %s field must contain a valid URL.',
+            valid_phone: 'The %s field must contain a valid phone number'
         },
         callback: function(errors) {
 
@@ -60,7 +61,8 @@
         ipRegex = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i,
         base64Regex = /[^a-zA-Z0-9\/\+=]/i,
         numericDashRegex = /^[\d\-\s]+$/,
-        urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
+        urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+        phoneRegex = /^(\+?1-?)?(\([2-9]([02-9]\d|1[02-9])\)|[2-9]([02-9]\d|1[02-9]))[\-\s{1}]?[2-9]([02-9]\d|1[02-9])[\-\s{1}]?\d{4}$/;
 
     /*
      * The exposed public object to validate a form:
@@ -482,6 +484,10 @@
 
         valid_url: function(field) {
             return (urlRegex.test(field.value));
+        },
+      
+        valid_phone: function(field) {
+            return (phoneRegex.test(field.value));
         },
 
         valid_credit_card: function(field){
